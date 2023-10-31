588fa61f00478f110bc5f062eb52b7242a6ebd65
jespy2 <jespy2@gmail.com>
2023-10-31 16:23:40 -0600

    added contact form to contact page

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import { Box, Link } from '@mui/material';
 
+import { ContactForm } from '../../components/contact-form/ContactForm';
+
 import LinkedInLogo from '../../images/logos/linkedin.png';
 import GithubLogo from '../../images/logos/github.png';
 import { Footer } from '../../components/footer/Footer';
@@ -11,6 +13,10 @@ import styles from './Contact.module.scss';
 export const Contact = () => {
   return (
     <Box className={styles.homeContainer} >
+      <Box className={styles.contactContainer} >
+        <ContactForm />
+      </Box>
+
       <Box className={styles.linksContainer}>
         <Link href='https://github.com/jespy2' target='_blank' ><img src={GithubLogo} /></Link>
         <Link href='https://www.linkedin.com/in/jamesespy/' target='_blank' ><img src={LinkedInLogo} /></Link>        
