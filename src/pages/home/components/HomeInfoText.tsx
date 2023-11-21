2da4736ab40c86789e3adacc14a9e8ea0e86ecde
jespy2 <jespy2@gmail.com>
2023-11-21 08:17:18 -0700

    componentizing modals: Home page

---
+++ src/pages/home/components/HomeInfoText.tsx
@@ -0,0 +1,46 @@
+import { Box, Typography } from "@mui/material";
+import styles from "../Home.module.scss";
+
+export const HomeInfoText = () => {
+
+	return (
+		<>
+      <Box className={styles.homeInfoBodyText}>
+        <Typography variant='body1' display='block'>
+          I build captivating front-end applications
+          that not only look great but also function seamlessly because they
+          are built on elegantly designed backends. What sets me apart is my
+          unique blend of technical prowess and a profound understanding of
+          business, communication, and empathetic team building.
+        </Typography>
+        <br />
+        <Typography variant='body1' display='block'>
+          With years of hands-on experience in the software development
+          world, I've honed my skills in creating user-friendly interfaces
+          that leave a lasting impression served by well thought out,
+          reliable backends. But my journey doesn't stop at the code; I've
+          also navigated the complexities of business, from startups to
+          established enterprises, and understand how software can drive
+          growth and innovation.
+        </Typography>
+        <br />
+        <Typography variant='body1'>
+          Communication is at the heart of every successful project, and I
+          excel at bridging the gap between technical and non-technical
+          stakeholders, ensuring everyone's voice is heard. I firmly believe
+          that building great software is a team effort, and I've cultivated
+          a collaborative, empathetic approach to team building that fosters
+          creativity and drives results.
+        </Typography>
+        <br />
+        <Typography variant='body1'>
+          In a rapidly evolving tech landscape, you need someone who can
+          translate your vision into elegant, functional, and scalable
+          solutions while seamlessly integrating with your business
+          objectives. I'm that person, and I'm here to help your team shine.
+          Let's create something incredible together.
+        </Typography>
+      </Box>
+		</>
+	);
+};
--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -1,31 +1,32 @@
-import { PropsWithChildren, useState } from 'react';
-import { Box } from '@mui/material';
+import { PropsWithChildren, useState } from "react";
+import { Box } from "@mui/material";
 
-import { SideBar } from './components/SideBar';
-import { Mosaic } from './components/Mosaic';
-import { Modal } from './components/modal/Modal';
-import { Footer } from '../../components/footer/Footer';
+import { SideBar } from "./components/SideBar";
+import { Mosaic } from "./components/Mosaic";
+import { Modal } from "../../components/modal/Modal";
+import { Footer } from "../../components/footer/Footer";
 
-import { itemData } from './Portfolio.config';
-
-import styles from './Portfolio.module.scss';
+import styles from "./Portfolio.module.scss";
 
 export const Portfolio = () => {
-  const [modalIsOpen, setModalIsOpen] = useState<PropsWithChildren>();
+	const [modalIsOpen, setModalIsOpen] = useState<PropsWithChildren>();
 
-  const closeModal = () => {
-    setModalIsOpen(undefined)
-  }
+	const closeModal = () => {
+		setModalIsOpen(undefined);
+	};
 
-  return (
-    <Box className={styles.portfolioContainer} >
-      <Box className={styles.portfolioBackgroundFilter} ></Box>
-      <SideBar />
-      <Mosaic setModalIsOpen={setModalIsOpen} />  
-      {modalIsOpen && 
-        < Modal children={modalIsOpen as PropsWithChildren} closeModal={closeModal} />
-      }    
-      <Footer page='portfolio' />
-    </Box>
-  );
+	return (
+		<Box className={styles.portfolioContainer}>
+			<Box className={styles.portfolioBackgroundFilter}></Box>
+			<SideBar />
+			<Mosaic setModalIsOpen={setModalIsOpen} />
+			{modalIsOpen && (
+				<Modal
+					children={modalIsOpen as PropsWithChildren}
+					closeModal={closeModal}
+				/>
+			)}
+			<Footer page='portfolio' />
+		</Box>
+	);
 };
