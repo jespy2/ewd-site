c0c41d8e81ffef9471e51cffc7f52f4e3e37d0d1
jespy2 <jespy2@gmail.com>
2023-11-27 12:25:53 -0700

    style fixes for portfolio modals

--- src/pages/portfolio/Portfolio.config.tsx
+++ src/pages/portfolio/Portfolio.config.tsx
@@ -26,8 +26,9 @@ export const itemData = [
     rows: 2,
     cols: 3,
     children: 
-      <>
+      <div className={styles.portfolioItemContent}>
         <Typography variant='h4' >GoMenu</Typography>
+        <div className={styles.portfolioModalCardHero} style={{backgroundImage: `url(${GomenuScreen})`}} />
         <Typography variant='body1' >After spending the past two years as a frontend dev, this project is me getting back to my roots in fullstack engineering, as well as flexing some of my skills that have matured in recent years.  I wanted something that had some interesting challenges in designing the UI and in the service layer, so I had the idea for an app that would take a URL for a recipe, scrape the page and enter the data into a form for the user to submit as a recipe card that gets added to the "cookbook".  A user is then able to add comments and ratings to their recipes, as well as find them easily through search and filtering.</Typography><br />
         <Typography variant='body1' >I used Figma for wireframing, Inkskape for icons and logos, and built a Jira board to track tasks and progress, along with managing deadlines. The frontend is built in React/Typescript and styled with SCSS and Material UI.  I built the server using Node/Express, which handles the CRUD functionality and webscraping.  I applied cloudbased MongoDB for the database.  Search and filtering is handled by Atlas search and server-side middleware.</Typography><br />
         <Typography variant='body1' >This project should be ready for MVP release very soon, and I have a roadmap for adding features and quality of life updates soon after.</Typography><br />
@@ -65,7 +66,7 @@ export const itemData = [
             Check back--you will be able to try out the app in your browser upon release        
           </Link>
         </Box>
-      </>
+      </ div>
   },
   {
     img: StorialScreen,
@@ -74,9 +75,10 @@ export const itemData = [
     rows: 2,
     cols: 2,
     children: 
-      <>
+      <div className={styles.portfolioItemContent}>
         <Typography variant='h4' >Storial</Typography>
         
+        <div className={styles.portfolioModalCardHero} style={{backgroundImage: `url(${StorialScreen})`}} />
         <Typography variant='body1' >This was for an assessment I did in 2021 for a role I ended up taking.  The prompt was the following:</Typography>
         <Typography variant='body2' sx={{ fontStyle: 'italic' }} >Using the UI framework of your choice, build a UI around a hypothetical RESTful library API. Assume this API returns a list of books in a library, and allows for CRUD operations on a single book. The application should support those endpoints.</Typography><br />
         <Typography variant='body1' >Because this was a frontend-only role, I was only tasked with building the UI, but decided to build a fullstack app so that the reviewing team could punch through the final product.  I started with using Adobe products to build the branding package, including name, logo, color palate and wireframing.  The frontend was in React, and I took the opportunity to try out Tailwind.css (which I enjoyed and ended up using quite a bit in my new role).  The backend was built with Node/Express and MongoDB. </Typography><br />
@@ -87,7 +89,7 @@ export const itemData = [
             Review the code and run the app on your machine (documentation and quick start guide in the README)
           </Link>
         </Box>
-      </>
+      </ div>
   
   },
   {
@@ -97,8 +99,10 @@ export const itemData = [
     rows: 2,
     cols: 2,
     children: 
-      <>
+      <div className={styles.portfolioItemContent}>
         <Typography variant='h4' >City Picker</Typography>
+
+        <div className={styles.portfolioModalCardHero} style={{backgroundImage: `url(${CityPickerScreen})`}} />
         <Typography variant='body1' >This was one of my first app projects in React (as opposed to "static" websites, which I'd been building for a few years).  The concept was to guide users through various data to determine what city would be the best fit for them.  The code is a bit rough, as I was tackling a bunch of new tech and concepts.  This is a relatively simple project in retrospect, but it was an important step for me in bringing together much of what I'd learned on my path to becoming an enterprise-level engineer (transitioning from freelance website design).</Typography><br />
 
         <Typography variant='body1' >My intention with this project was to highlight my front-end skills. There was some interesting challenges with the data (collection, processing and algorithm development), but didn't want to make that too central to this addition to my portfolio.</Typography><br />
@@ -144,7 +148,7 @@ export const itemData = [
           </Link>
         </Box>
         </Box>
-      </>
+      </ div>
   },
   {
     img: RedisHawkScreen,
@@ -153,8 +157,9 @@ export const itemData = [
     rows: 2,
     cols: 3,
     children: 
-      <>
+      <div className={styles.portfolioItemContent}>
         <Typography variant='h4' >redis-hawk</Typography>
+        <div className={styles.portfolioModalCardHero} style={{backgroundImage: `url(${RedisHawkScreen})`}} />
         <Typography variant='body1' >I was part of the open-source team that developed redis-hawk, an app that allows users to monitor the keyspace and its events within all databases of any number of deployed Redis instances.
         </Typography><br />
         <Typography variant='body1' >This was a great opportunity to work with some awesome engineers that allowed me to focus on the backend.  My contribution was adding some fun algorithms and data structures, as well as basic routing to the server-side middleware.</Typography><br />
@@ -164,6 +169,6 @@ export const itemData = [
             Review the code
           </Link>
         </Box>
-      </>
+      </ div>
   },
 ]
